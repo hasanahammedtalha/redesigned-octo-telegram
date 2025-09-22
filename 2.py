20ce61480031157131f17771530f865dc6e59d9b
Hasan A.H Talha <156737546+hasanahammedtalha@users.noreply.github.com>
2025-09-22 14:31:58 +0600

    Update v3s 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,10 @@
+import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
 
-
 # =======================
 # Bot Token
-TOKEN = "7884768889:AAHyXrH1YDwwPhHP-pZn9R5ukWhFPB4xG2U"
-
+TOKEN = os.environ.get("BOT_TOKEN")  # Render Environment Variable এ রাখো
 
 
 # =======================
@@ -45,19 +44,24 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.edit_message_text(text="👉 PROXY DETAILS \n NAME: USA(BUFALIO)[PRO] \n SPEED: 3MB ↓↑ \n ISSUE: ❌ NO ISSUE \n ID SUSPEND: ⛔ NO \n LIMIT: 1GB \n USES: 24/7 \n PRICE: 40 BDT/0.38 USD \n\n\n 🛒 FOR BUY 🛒\n PAY 40 BTD ON BKASH/NAGAD\n 01796095176\n GIVE SCREENSHOT OF PAYMENT\n BOT: @sell4ubd_bot\n CHANNEL: @sell4u_market")
 
 
-
 # =======================
 # Main function
-def main():
+if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(button_handler))
 
-    print("🚀 Bot is running...")
-    app.run_polling()
+    print("🚀 Bot is running with webhook...")
 
-# =======================
-if __name__ == "__main__":
-    main()
+    PORT = int(os.environ.get("PORT", 10000))
+
+    # পুরনো webhook clear করো
+    app.bot.delete_webhook()
+
+    # Render এর domain বসাও (নিজের domain দিয়ে পরিবর্তন করো)
+    RENDER_URL = "https://redesigned-octo-telegram-12x.onrender.com/"
+    app.bot.set_webhook(RENDER_URL)
+
+    app.run_webhook(listen="0.0.0.0", port=PORT, webhook_url=RENDER_URL)
